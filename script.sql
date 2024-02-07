eec5fc88919a880fccac1cdbc7a14c2794b8732b
bruno <brunogonzaga10@hotmail.com>
2024-02-07 12:32:19 -0300

    atualizacao script sql

--- script.sql
+++ script.sql
@@ -1,9 +1,28 @@
 -- Coloque scripts iniciais aqui
 
---INSERT INTO clientes (nome, limite)
---VALUES
---('o barato sai caro', 1000 * 100),
---('zan corp ltda', 800 * 100),
---('les cruders', 10000 * 100),
---('padaria joia de cocaia', 100000 * 100),
---('kid mais', 5000 * 100);
+CREATE TABLE cliente (
+	id int4 NOT NULL,
+	limite int8 NULL,
+	saldo int8 DEFAULT 0,
+	CONSTRAINT cliente_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE transacao (
+	id int4 NOT NULL,
+	descricao varchar(255) NULL,
+	id_cliente int4 NOT NULL,
+	realizada_em timestamptz(6) NULL,
+	tipo varchar(255) NULL,
+	valor int8 NULL,
+	CONSTRAINT transacao_pkey PRIMARY KEY (id)
+);
+
+CREATE SEQUENCE transacao_seq INCREMENT BY 50;
+
+INSERT INTO cliente (id, limite)
+VALUES
+(1, 1000 * 100),
+(2, 800 * 100),
+(3, 10000 * 100),
+(4, 100000 * 100),
+(5, 5000 * 100);
