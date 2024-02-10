5f2fdebbb2743b85fad383f4070a2a01d2e9dff4
bruno <brunogonzaga10@hotmail.com>
2024-02-10 12:03:50 -0300

    scripts sql

--- 1-init.sql
+++ 1-init.sql
@@ -1,14 +1,14 @@
 -- Coloque scripts iniciais aqui
 
-CREATE TABLE cliente (
+CREATE UNLOGGED TABLE cliente (
 	id int4 NOT NULL,
 	limite int8 NULL,
 	saldo int8 DEFAULT 0,
 	CONSTRAINT cliente_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE transacao (
-	id int4 NOT NULL,
+CREATE UNLOGGED TABLE transacao (
+	id SERIAL NOT NULL,
 	descricao varchar(255) NULL,
 	id_cliente int4 NOT NULL,
 	realizada_em timestamptz(6) NULL,
@@ -17,8 +17,6 @@ CREATE TABLE transacao (
 	CONSTRAINT transacao_pkey PRIMARY KEY (id)
 );
 
-CREATE SEQUENCE transacao_seq INCREMENT BY 50;
-
 INSERT INTO cliente (id, limite)
 VALUES
 (1, 1000 * 100),
@@ -26,3 +24,10 @@ VALUES
 (3, 10000 * 100),
 (4, 100000 * 100),
 (5, 5000 * 100);
+
+
+--select ("cred" - "deb") as "saldo", t1.id_cliente from
+--(select sum(valor) as "cred", id_cliente from transacao where tipo='c' group by id_cliente) as t1
+--join (select sum(valor) as "deb", id_cliente from transacao where tipo='d' group by id_cliente) as t2
+--on t1.id_cliente = t2.id_cliente
+--order by t1.id_cliente asc
---
+++ funcao.sql
@@ -0,0 +1,28 @@
+create or replace function cria_transacao(id_cliente int, t_valor bigint, t_tipo varchar, t_descricao varchar) returns table(l bigint, s bigint)
+as $$
+declare
+	novoSaldo bigint;
+	novoValor bigint;
+	client cliente%rowtype;
+begin
+	select * into client from cliente c where c.id = id_cliente for update;
+	if not found then
+--		raise exception 'id inexistente';
+		return query select cast(-1 as BIGINT), cast(-1 as BIGINT);
+	end if;
+	if t_tipo = 'd' then
+		novoValor := t_valor * -1;
+	else
+		novoValor := t_valor;
+	end if;
+	novoSaldo := client.saldo + novoValor;
+	if (abs(novoSaldo) - client.limite) > 0 and t_tipo = 'd' then
+--		raise exception 'limite insuficiente';
+		return query select cast(-2 as BIGINT), cast(-2 as BIGINT);
+	else
+		insert into transacao(id,valor, tipo, descricao, realizada_em, id_cliente) values(default,t_valor, t_tipo, t_descricao, now(), id_cliente);
+		return query update cliente c set saldo = novoSaldo where id=id_cliente returning c.limite, c.saldo;
+	end if;
+end
+$$
+language plpgsql;
